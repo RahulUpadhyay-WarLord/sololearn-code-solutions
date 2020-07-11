cbc1c0db4f840c1373be99042f230ac6ed25a4ee
Trifonov Dmitriy <62952967+Djivs@users.noreply.github.com>
2020-07-11 19:59:55 +0300

    argentina.cpp perfect codestyle

--- Code coach problems/Easy/C++/Argentina.cpp
+++ Code coach problems/Easy/C++/Argentina.cpp
@@ -1,14 +1,26 @@
+/*
+ * Sololearn Argentina Easy Task Solution
+ * Djivs, 2020
+*/
 #include <iostream>
 using namespace std;
-
-int main() {
-	//p - pesos, d - dollars
-	int p, d;
-	cin >> p >> d;
+int main()
+{
+	int pesos, dollars;
+	//variables to store pesos and dollars values
+	
+	cin >> pesos >> dollars;
+	//input values
+	
+	pesos /= 50; 
 	//convert pesos to dollars
-	p /= 50;
-	//if pesos converted value is less than dollars value,
-	//print "Pesos". Else, print "Dollars"
-	cout << ((p < d) ? "Pesos" : "Dollars") ;
+	
+	cout << ( ( pesos < dollars ) ? "Pesos" : "Dollars" );
+	/*
+	 * if pesos converted value is less than dollars value,
+	 * print "Pesos". Else, print "Dollars"
+	*/
+	
 	return 0;
+	//end programm
 }
