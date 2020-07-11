b02d4d0c81e03ca5ca49fa52ad9b1e54f7b07c7e
Trifonov Dmitriy <62952967+Djivs@users.noreply.github.com>
2020-07-11 17:15:23 +0300

    Add comments to Argentina.cpp

--- Code coach problems/Easy/C++/Argentina.cpp
+++ Code coach problems/Easy/C++/Argentina.cpp
@@ -2,9 +2,13 @@
 using namespace std;
 
 int main() {
+	//p - pesos, d - dollars
 	int p, d;
 	cin >> p >> d;
+	//convert pesos to dollars
 	p /= 50;
+	//if pesos converted value is less than dollars value,
+	//print "Pesos". Else, print "Dollars"
 	cout << ((p < d) ? "Pesos" : "Dollars") ;
 	return 0;
 }
