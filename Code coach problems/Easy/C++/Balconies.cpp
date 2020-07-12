4f2eeb1ebd55af9477ea503bfb95065ea65e525c
Trifonov Dmitriy <62952967+Djivs@users.noreply.github.com>
2020-07-12 15:25:54 +0300

    Add comments to Balconies.cpp

--- Code coach problems/Easy/C++/Balconies.cpp
+++ Code coach problems/Easy/C++/Balconies.cpp
@@ -1,16 +1,32 @@
+/*
+ * Balconies Solilearn task solution
+ * Djivs, 2020
+*/
 #include <iostream>
 #include <string>
 using namespace std;
 int main()
 {
-	int b[2] = {0, 0};
-	string a;
+        //array with apartments areas
+	int areas[2] = {0, 0};
+        //string to input data about apartments
+	string buf;
+        //Then, 2 times(for apartmant A and B):
 	for(int i = 0; i < 2; i++)
 	{
-		cin >> a;
-		auto f = a.find_first_of(",");
-		b[i] = stoi(a.substr( 0, f ) ) * stoi( a.substr( f+1, a.size() ) );
+                //Get data about current apartment
+		cin >> buf;
+                //Find separator(',') to split string
+		auto f = buf.find_first_of(",");
+                // Current area equals first number
+                // multiplied by second number
+                //First number - before ',' , second - after
+		areas[i] = stoi(buf.substr( 0, f ) ) * stoi( buf.substr( f+1, a.size() ) );
 	}
 	cout << "Apartment ";
-	cout << (b[0] > b[1] ? 'A' : 'B');
+        //If first area is bigger than second, answer is A
+        //Else, answer is B
+	cout << (areas[0] > areas[1] ? 'A' : 'B');
+        //end programm
+        return 0;
 }
