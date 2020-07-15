5f3659f9ed0961caf5c5977ebd50a21f537facb1
Trifonov Dmitriy <62952967+Djivs@users.noreply.github.com>
2020-07-15 15:23:32 +0300

    Create Fruit_Bowl.java

---
+++ Code coach problems/Easy/Java/Fruit_Bowl.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Program
+{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int fruit = input.nextInt();
+        System.out.println(fruit/2/3);
+
+       
+    }
+}
