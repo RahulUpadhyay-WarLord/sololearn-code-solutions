0c1bd4f66379884e12ac9ee339b53c93bebc94ac
Trifonov Dmitriy <62952967+Djivs@users.noreply.github.com>
2020-07-15 15:24:23 +0300

    First Java Solution!

--- Code coach problems/Easy/Java/Fruit_Bowl.java
+++ Code coach problems/Easy/Java/Fruit_Bowl.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 
 public class Program
 {
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int fruit = input.nextInt();
         System.out.println(fruit/2/3);
 
-       
     }
 }
